4f49fecb40b317060ecc22d2f54b6f62b6de89f6
Arun <arunhari3210@gmail.com>
2021-02-14 21:49:39 +0530

    Added some more comments in the backend

--- backend/memes/models.py
+++ backend/memes/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 
+# Model to store memes
 class Meme(models.Model):
     name = models.CharField(max_length=100)
     caption = models.CharField(max_length=500)
+    # Basic URL validation is added in backend considering request from sources other than frontend
     url = models.CharField(max_length=1000, validators=[
         RegexValidator(
             regex='(http|https)://',
--- backend/memes/serializers.py
+++ backend/memes/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Meme
 
 
+# Serializes the data
 class MemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meme
         fields = '__all__'
 
 
+# Serializes the partial data to receive PATCH request
 class PartialMemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meme
